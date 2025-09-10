77603effd442b578d53650879626ca92db70a337
Your Name <you@example.com>
2025-09-09 17:59:37 -0700

    UI change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,6 @@ export default function Footer() {
         <a href="https://github.com/AceSpadee" target="_blank" rel="noopener noreferrer">
             Github repo
         </a>
-        <a href="https://www.linkedin.com/in/peyton-iverson-3a6534327/" target="_blank" rel="noopener noreferrer">
-            Linked in
-        </a>
         <p>
           2024. All Rights Reserved to Peyton Iverson
         </p>
