c716f11dc3b5b8d4d2a7df0823ba554258e4447d
Your Name <you@example.com>
2026-03-10 17:17:03 -0700

    switched around projects

--- src/components/UI/ProjectData.jsx
+++ src/components/UI/ProjectData.jsx
@@ -7,10 +7,18 @@ import PathTracker from "../../assets/img4.png";
 
 const projectsData = [
   {
-    title: "Project_1",
-    image: Project1,
-    repoLink: "https://github.com/AceSpadee/Project-1",
-    liveLink: "https://acespadee.github.io/Project-1/",
+    title: "Music-Player",
+    image: MusicPlayer,
+    repoLink: "",
+    liveLink: "https://music-player-s59b.onrender.com",
+    private: true,
+    mockSite: "Frontend Demo Only – No Backend",
+  },
+  {
+    title: "Rooftop-Cat",
+    image: RoofTopCat,
+    repoLink: "https://github.com/AceSpadee/ambient-loader",
+    liveLink: "https://ambient-loader.onrender.com",
     private: false,
   },
   {
@@ -21,35 +29,27 @@ const projectsData = [
     private: false,
   },
   {
-    title: "Techblog-Website",
-    image: Techblog,
-    repoLink: "https://github.com/AceSpadee/Tech-Blog-website",
-    liveLink: "https://tech-blog-website-775s.onrender.com",
-    private: false,
-  },
-  {
-    title: "Rooftop-Cat",
-    image: RoofTopCat,
-    repoLink: "https://github.com/AceSpadee/ambient-loader",
-    liveLink: "https://ambient-loader.onrender.com",
-    private: false,
-  },
-  {
-    title: "Music-Player",
-    image: MusicPlayer,
-    repoLink: "",
-    liveLink: "https://music-player-s59b.onrender.com",
-    private: true,
-    mockSite: "Frontend Demo Only – No Backend",
-  },
-  {
     title: "Path-Tracker",
     image: PathTracker,
     repoLink: "",
     liveLink: "",
     private: true,
     mockSite: "🔒 Private Project – Code & Demo Not Public"
-  }
+  },
+  {
+    title: "Project_1",
+    image: Project1,
+    repoLink: "https://github.com/AceSpadee/Project-1",
+    liveLink: "https://acespadee.github.io/Project-1/",
+    private: false,
+  },
+  {
+    title: "Techblog-Website",
+    image: Techblog,
+    repoLink: "https://github.com/AceSpadee/Tech-Blog-website",
+    liveLink: "https://tech-blog-website-775s.onrender.com",
+    private: false,
+  },
 ];
 
 export default projectsData;
